8e4f5da71b53b173c01d4961267a6337175c4658
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-09 14:20:09 +0800

    Added motion to skills bar

---
+++ main.js
@@ -0,0 +1,19 @@
+let items = document.getElementsByClassName("skills-item")
+for(let i = 0; i < items.length; i++){
+    let item = items[i];
+    let percentage = item.getAttribute('data-percent');
+    item.style.width = 0;
+    let interval = setInterval(function () {
+        let currentWidth = parseFloat(item.style.width.replace('px', ''));
+        currentWidth = currentWidth + item.parentElement.clientWidth / 80;
+        if(currentWidth > item.parentElement.clientWidth){
+            currentWidth = item.parentElement.clientWidth;
+        }
+        item.style.width = currentWidth + 'px';
+        console.log(item.style.width, currentWidth)
+        console.log(item.parentElement.clientWidth)
+        if (parseInt(item.style.width) >= item.parentElement.clientWidth * percentage) {
+            clearInterval(interval);
+        }
+    }, 1)
+}
