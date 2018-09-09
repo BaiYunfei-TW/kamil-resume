f3aa09d914e4e1cee5d02df4b9312af1ec38b67d
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-09 14:27:03 +0800

    Refactor: refact javascript for bar motion

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-let items = document.getElementsByClassName("skills-item")
-for(let i = 0; i < items.length; i++){
-    let item = items[i];
-    let percentage = item.getAttribute('data-percent');
-    item.style.width = 0;
-    let interval = setInterval(function () {
-        let currentWidth = parseFloat(item.style.width.replace('px', ''));
-        currentWidth = currentWidth + item.parentElement.clientWidth / 80;
-        if(currentWidth > item.parentElement.clientWidth){
-            currentWidth = item.parentElement.clientWidth;
-        }
-        item.style.width = currentWidth + 'px';
-        if (parseInt(item.style.width) >= item.parentElement.clientWidth * percentage) {
-            clearInterval(interval);
-        }
-    }, 1)
-}
+let barsWithEnterMotion = ['skills-item', 'language-icon-level'];
+
+barsWithEnterMotion.forEach(bar => {
+    let items = document.getElementsByClassName(bar)
+    for(let i = 0; i < items.length; i++){
+        let item = items[i];
+        let percentage = item.getAttribute('data-percent');
+        let parentsWidth = item.parentElement.clientWidth;
+        item.style.width = '0px';
+        let interval = setInterval(function () {
+            let currentWidth = parseFloat(item.style.width.replace('px', ''));
+            currentWidth = currentWidth + item.parentElement.clientWidth / 80;
+            if(currentWidth > parentsWidth){
+                currentWidth = parentsWidth;
+            }
+            item.style.width = currentWidth + 'px';
+            if (parseInt(item.style.width) >= item.parentElement.clientWidth * percentage) {
+                clearInterval(interval);
+            }
+        }, 1)
+    }
+});
